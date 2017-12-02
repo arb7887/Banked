93770c70843a9577cd1c4743bb0716be7a16fd08
Andrew Baron (RIT Student) <arb7887@ad.rit.edu>
2017-12-01 23:03:18 -0500

    Scene updates, coin, done with day 1!

---
+++ Ludum-Dare-40/Assets/Scripts/CoinAnimation.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinAnimation : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.Rotate(0.0f, 0.0f, 30.0f * Time.deltaTime);
+	}
+}
---
+++ Ludum-Dare-40/Assets/Scripts/Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    private float h_acceleration = 10f;
+	// Update is called once per frame
+	void Update () {
+        Vector3 velocity = new Vector3(0f, 0f);
+        if (Input.GetKey(KeyCode.W)) velocity.z += h_acceleration * Time.deltaTime;
+        if (Input.GetKey(KeyCode.S)) velocity.z -= h_acceleration * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A)) velocity.x -= h_acceleration * Time.deltaTime;
+        if (Input.GetKey(KeyCode.D)) velocity.x += h_acceleration * Time.deltaTime;
+        Mathf.Clamp(velocity.x, 0, 10f);
+        Mathf.Clamp(velocity.z, 0, 10f);
+        gameObject.transform.Translate(velocity);
+    }
+}
