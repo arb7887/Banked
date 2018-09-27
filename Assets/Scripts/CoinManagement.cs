79302d53d1ad4c0e5b438eb8734a84eb79ab22ca
Andrew Baron (RIT Student) <arb7887@ad.rit.edu>
2018-09-27 10:46:32 -0400

    Did some work on CoinManagement, not finished

--- Assets/Scripts/CoinManagement.cs
+++ Assets/Scripts/CoinManagement.cs
@@ -1,19 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
 public class CoinManagement : MonoBehaviour {
+    private enum CoinType
+    {
+        Normal,
+        Negative,
+        Light
+    };
+    private List<CoinType> availableCoinTypes;
+    private CoinType currentCoinType;
+
     public int amountOfCoins;
     public GameObject coin;
     public GameObject scoreText;
     void Awake()
     {
+        availableCoinTypes = new List<CoinType>();
         amountOfCoins = 0;
     }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse1) && amountOfCoins > 0)
+        //TO DO
+        if (Input.GetKeyUp(KeyCode.Tab))
+        {
+
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse1) && amountOfCoins > 0)
         {
             GameObject newcoin = Instantiate(coin, transform.position - (transform.forward.normalized * 2.0f), Quaternion.identity);
             amountOfCoins--;
@@ -21,13 +37,13 @@ public class CoinManagement : MonoBehaviour {
             newcoin.GetComponent<CoinAnimation>().shouldAnimate = false;
             Vector3 velocity = -transform.forward.normalized * 2.0f;
             newcoin.GetComponent<Rigidbody>().velocity = velocity;
-            newcoin.GetComponent<Rigidbody>().angularVelocity = new Vector3(-4.0f, 0.0f, 0.0f);
+            newcoin.GetComponent<Rigidbody>().angularVelocity = transform.right * -4;
         }
         scoreText.GetComponent<TextMeshProUGUI>().SetText(amountOfCoins.ToString());
     }
     void OnTriggerEnter(Collider c)
     {
-        if (c.gameObject.tag == "Coin")
+        if (c.gameObject.tag.Contains("Coin"))
         {
             c.gameObject.SetActive(false);
             Destroy(c.gameObject);
